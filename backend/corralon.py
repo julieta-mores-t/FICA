78f82b4fa70b4a77e26c82cabea99acf6054534f
Damián Márquez <69488200+damianjmarquez@users.noreply.github.com>
2024-10-09 16:40:28 -0300

    ,

--- backend/corralon.py
+++ backend/corralon.py
@@ -35,7 +35,9 @@ def agregar_usuario():
     con = mysql.connector.connect(**base)
     cursor = con.cursor()
 
-    cursor.execute("INSERT INTO empleados (nombre,apellido,dni,fechaNacimiento,direccion,mail,telefono,usuario,clave,puesto) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(nombre,apellido,dni,fecha_nacimiento,direccion,mail,telefono,usuario,clave,puesto))
+    cursor.execute(
+                    "INSERT INTO empleados (nombre,apellido,dni,fechaNacimiento,direccion,mail,telefono,usuario,clave,puesto) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+                    (nombre,apellido,dni,fecha_nacimiento,direccion,mail,telefono,usuario,clave,puesto))
     con.commit()
 
     cursor.close()
@@ -57,9 +59,37 @@ def agregar_material():
 
     cursor.execute("INSERT INTO deposito (material,cantidad,precio) VALUES (%s,%s,%s)",(material,cantidad,precio))
     con.commit()
+    cursor.close()
+    con.close()
     ultimo_dato = cursor.lastrowid
     return jsonify({"Mensaje":"Material ingresado","id": ultimo_dato}), 201
 
+
+#---------------se agrega compradores---------------------------
+@aplicacion.route("/api/agregar_comprador", methods=["POST"])
+def agregar_comprador():
+    data = request.get_json()
+    nombre = data.get("nombre")
+    apellido = data.get("apellido")
+    dni = data.get("dni")
+    fecha_nacimiento = data.get("fecha_nacimiento")
+    direccion = data.get("direccion")
+    email = data.get("email")
+    telefono = data.get("telefono")
+
+    con = mysql.connector.connect(**base)
+    cursor = con.cursor()
+
+    cursor.execute(
+                    "INSERT INTO compradores (nombre,apellido,dni,fecha_nacimiento,direccion,email,telefono) VALUES (%s,%s,%s,%s,%s,%s,%s)",
+                    (nombre,apellido,dni,fecha_nacimiento,direccion,email,telefono))
+    con.commit()
+
+    con.close()
+    cursor.close()
+
+    ultimo_dato = cursor.lastrowid
+    return({"Mensaje": "Comprador agregado","id": ultimo_dato}),201
 
 
 

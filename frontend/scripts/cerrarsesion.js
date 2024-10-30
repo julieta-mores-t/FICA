cb46644322573f09d793d35c1fbdf9c1ede5a813
Julieta Mores <julieta.mores.t@gmail.com>
2024-10-30 17:06:12 -0300

    navbar

---
+++ frontend/scripts/cerrarsesion.js
@@ -0,0 +1,4 @@
+document.getElementById("btn-salir").addEventListener("click", function() {
+    // Opcional: agrega lógica de cierre de sesión aquí, como limpiar tokens o cookies.
+    window.location.href = "../index.html"; // Redirige a index.html
+});
